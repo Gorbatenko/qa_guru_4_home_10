9e07f71dc9f9cf08710f303459e0d5d75186cb04
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-03-25 12:59:21 +0200

    add TestRunConfig

--- src/test/java/config/TestRunConfig.java
+++ src/test/java/config/TestRunConfig.java
@@ -2,9 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources("classpath:testrun.properties")
 public interface TestRunConfig extends Config {
     @Key("task")
     @DefaultValue("test")
     String getTask();
-
 }
